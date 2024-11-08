7a065ec024c16702af9984bf31170d969d21bec6
Reox <reox.jai@gmail.com>
2024-11-08 12:17:21 +0100

    arreglo carpetas

--- DWEC/worksheet4b/Calculadora2/index.js
+++
@@ -1,7 +0,0 @@
-const contenedor = document.getElementById("contenedor");
-
-const boton = document.createElement("button"); 
-boton = document.addEventListener("click", function() {
-    console.log("Has pulsado el boton " + boton.id);
-});
-    
