f98688ed231847b528e7b91117a4aab5f07e91c2
Reox <reox.jai@gmail.com>
2025-01-24 11:11:10 +0100

    vue2

---
+++ DWEC/SegundoTrim/Vue/Proyect/componentes/Register.vue
@@ -0,0 +1,10 @@
+
+
+async function register(email, password){
+    try{
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      console.log("Usuario registrado: ", userCredential.user);
+    }catch (error){
+      console.error("Error al registrarse: ", error.message);
+    }
+}
