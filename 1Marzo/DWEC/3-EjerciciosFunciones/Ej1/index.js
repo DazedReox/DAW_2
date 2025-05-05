e450b755f1d59ddd7db8a8440e436e079ac622fa
Reox <reox.jai@gmail.com>
2025-05-05 17:42:37 +0200

    functions

---
+++ 1Marzo/DWEC/3-EjerciciosFunciones/Ej1/index.js
@@ -0,0 +1,9 @@
+const array = [1,2,3,4,5];
+
+function promedio(array){
+    const media = 0;
+    for(let i = 0; i<array.length;i++){
+        media = array[i];
+    }
+    return media/array.length;
+}
